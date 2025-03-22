c3340b3147647b999defe236f27e22717962760f
Denis Kolaj <deniskolaj2@gmail.com>
2025-03-23 00:01:35 +0100

    create Credentials model

---
+++ src/Models/Credentials.cs
@@ -0,0 +1,11 @@
+﻿namespace Keylume.Models
+{
+    public class Credentials(string identifier, string username, string password, string notes = "")
+    {
+        public string Identifier { get; } = identifier;
+        public string Username { get; } = username;
+        public string Password { get; } = password;
+        public string Notes { get; } = notes;
+        public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    }
+}
