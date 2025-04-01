7e99d5cc8bee273773540bbe16f8192983fa312f
Denis Kolaj <deniskolaj2@gmail.com>
2025-04-01 23:40:05 +0200

    add PasswordGenerator class for generating random passwords

---
+++ src/Utils/PasswordGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace Keylume.Utils
+{
+    public static class PasswordGenerator
+    {
+        private const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+        private const string Numbers = "0123456789";
+        private const string Symbols = "!@#$%^&*()-_=+<>?";
+
+        public static string Generate(int length = 16, bool includeSymbols = true)
+        {
+            string validChars = Uppercase + Lowercase + Numbers;
+            if (includeSymbols) validChars += Symbols;
+
+            Random random = new Random();
+            return new string(Enumerable.Repeat(validChars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
