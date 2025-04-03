3793cbe9621b29252bdce2fe013e91e2b68235e7
Denis Kolaj <deniskolaj2@gmail.com>
2025-04-04 00:14:29 +0200

    Add EncryptionService class to handle AES encryption & decryption

---
+++ src/Services/EncryptionService.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Keylume.Services
+{
+    public class EncryptionService
+    {
+        private readonly byte[] key;
+        private readonly byte[] iv;
+
+        public EncryptionService(string keyString, string ivString)
+        {
+            key = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(keyString));
+            iv = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(ivString));
+        }
+
+        public string Encrypt(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                {
+                    byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+                    byte[] encryptedBytes = encryptor.TransformFinalBlock(inputBytes, 0, inputBytes.Length);
+                    return Convert.ToBase64String(encryptedBytes);
+                }
+            }
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                    return Encoding.UTF8.GetString(decryptedBytes);
+                }
+            }
+        }
+    }
+}
