9ae2e857847b2c9ddf5a73cf5a385f0e7df0e56f
Denis Kolaj <deniskolaj2@gmail.com>
2025-04-15 23:50:48 +0200

    implement save and delete functions

--- src/Services/PasswordManager.cs
+++ src/Services/PasswordManager.cs
@@ -32,16 +32,31 @@ namespace Keylume.Services
                 return new List<Credentials>();
             }
         }
-        
+
+        private void SaveCredentials()
+        {
+            string json = JsonSerializer.Serialize(credentials);
+            string encryptedJson = encryptionService.Encrypt(json);
+            File.WriteAllText(filePath, encryptedJson);
+        }
+
         public void StorePassword(string identifier, string username, string password, string notes = "")
         {
             credentials.Add(new Credentials(identifier, username, encryptionService.Decrypt(password), notes));
+            SaveCredentials();
         }
+
 
         public string RetrievePassword(string identifier)
         {
             var credential = credentials.FirstOrDefault(x => x.Identifier == identifier);
             return credential != null ? encryptionService.Decrypt(credential.Password) : "Not found";
+        }
+
+        public void DeletePassword(string identifier)
+        {
+            credentials.RemoveAll(c => c.Identifier == identifier);
+            SaveCredentials();
         }
     }
 }
